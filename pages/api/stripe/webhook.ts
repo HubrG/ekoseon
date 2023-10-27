7db4ec4c61f3ec5500ff915fcaee08367dabf6a8
HubrG <hubrgiorgi@gmail.com>
2023-10-27 19:43:36 +0200

    Checkout form finalize / webhook Stripe

--- pages/api/stripe/webhook.ts
+++ pages/api/stripe/webhook.ts
@@ -1,32 +1,45 @@
 // /pages/api/webhook.js
+import { prisma } from "@/lib/prisma";
 import { buffer } from "micro";
+import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2020-08-27",
+interface Invoice {
+  // ... autres propriétés
+  subscription: string;
+  payment_intent: string;
+}
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2022-11-15",
 });
 
 const endpointSecret = process.env.STRIPE_SIGNIN_SECRET;
-
+if (!endpointSecret) {
+  throw new Error("STRIPE_SIGNIN_SECRET is not defined.");
+}
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
-async function cancelSubscriptionAtPeriodEnd(subscriptionId) {
+async function cancelSubscriptionAtPeriodEnd(subscriptionId: any) {
   await stripe.subscriptions.update(subscriptionId, {
     cancel_at_period_end: true,
   });
-  // console.log(updatedSubscription);
 }
 
-const webhookHandler = async (req, res) => {
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const buf = await buffer(req);
     const sig = req.headers["stripe-signature"];
+
+    if (typeof sig !== "string") {
+      res.status(400).send("Invalid Stripe signature format");
+      return;
+    }
 
     let event;
 
@@ -37,73 +50,67 @@ const webhookHandler = async (req, res) => {
         endpointSecret
       );
     } catch (err) {
-      res.status(400).send(`Webhook Error: ${err.message}`);
+      if (err instanceof Error) {
+          res.status(400).send(`Webhook Error: ${err.message}`);
+      } else {
+          res.status(400).send(`Webhook Error: An unknown error occurred`);
+      }
       return;
     }
+    
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
     if (event.type === "invoice.payment_succeeded") {
-      // NOTE : FACTURE SUBSCRIPTION
-      const invoice = event.data.object;
-      const invoiceUrl = invoice.hosted_invoice_url;
+      // NOTE : PAYMENT INTENT SUBSCRIPTION
+      const invoice = event.data.object as Invoice;
       const subId = invoice.subscription;
-        const paymentIntent = invoice.payment_intent;
-        if (paymentIntent != null) {
-            // On vérifie avec Prisma combien il y a de subscribeId
-            await prisma.invoices.create({
-                data: {
-                    invoiceUrl: invoiceUrl,
-                    status: "paid",
-                    paymentIntent: paymentIntent,
-                    subscribeId: subId,
-                    amount: invoice.amount_due / 100,
-                },
-            });
-            const count = await prisma.invoices.count({
-                where: {
-                    subscribeId: subId,
-                    paymentIntent: {
-                        not: null
-                    }
-                },
-            });
-            if (count > 1) {
-                // On recherche le customer via son subscribeId sur Prisma
-                const customer = await prisma.order.findUnique({
-                    where: {
-                        stripePaymentIntent: subId,
-                    },
-                });
-                // ON récupère stripeSubscriptionMonth avec Prisma
-                const stripeSubscriptionMonth = customer.stripeSubscriptionMonth;
-                if (count == stripeSubscriptionMonth) {
-                    cancelSubscriptionAtPeriodEnd(subId);
-                }
-            }
-        }
+      const paymentIntent = invoice.payment_intent;
+      // 
+          await delay(10000);
+  
+      // On recherche l'ID de la commande
+      const orderId = await prisma.order.findFirst({
+        where: {
+          isSub: subId,
+        },
+      });
+      // Si elle existe...
+      if (paymentIntent) {
+          await prisma.payment.create({
+            data: {
+              status: "paid",
+              paymentIntent: paymentIntent,
+              orderId: orderId?.id,
+              amount: 100,
+            },
+          });
+          // On vérifie avec Prisma combien il y a de subscribeId
+          // const count = await prisma.invoices.count({
+          //   where: {
+          //     subscribeId: subId,
+          //     paymentIntent: {
+          //       not: null,
+          //     },
+          //   },
+          // });
+          // if (count > 1) {
+          //   // On recherche le customer via son subscribeId sur Prisma
+          //   const customer = await prisma.order.findUnique({
+          //     where: {
+          //       stripePaymentIntent: subId,
+          //     },
+          //   });
+          //   // ON récupère stripeSubscriptionMonth avec Prisma
+          //   const stripeSubscriptionMonth = customer.stripeSubscriptionMonth;
+          //   if (count == stripeSubscriptionMonth) {
+          //     cancelSubscriptionAtPeriodEnd(subId);
+          //   }
+          // }
+        
+      }
     }
-      
-   
-      
-     
-      
-    // if (event.type === "charge.succeeded") {
-    //   // NOTE : FACTURE CLASSIQUE
-    //   const charge = event.data.object;
-    //   if (charge.description == null) {
-    //     const chargeId = charge.payment_intent;
-    //     //   console.log(chargeId);
-    //     const invoiceUrl = charge.receipt_url;
-    //     // On cherche la commande dans la base de données, table ORDER, avec le stripePaymentIntent
-    //     await prisma.invoices.create({
-    //       data: {
-    //         invoiceUrl: invoiceUrl,
-    //         status: "paid",
-    //         paymentIntent: chargeId,
-    //         amount: charge.amount / 100,
-    //       },
-    //     });
-    //   }
-    // }
+
+  
 
     res.status(200).json({ received: true });
   } else {
