65a59c8dfcd4e2c99972d1be7adf5767e464c0ed
HubrG <hubrgiorgi@gmail.com>
2023-12-04 21:50:11 +0100

    Improvement sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,42 +6,45 @@ import {
 } from "@/src/query/blog.query";
 
 export default async function sitemap() {
- 
   const sitemap = [
     {
       url: "http://ekoseon.fr/raconter-ses-memoires/tarifs",
+      lastModified: new Date(),
     },
     {
       url: "http://ekoseon.fr/raconter-ses-memoires/fonctionnement",
+      lastModified: new Date(),
     },
     {
       url: "http://ekoseon.fr/raconter-ses-memoires/contact",
+      lastModified: new Date(),
     },
     {
       url: "http://ekoseon.fr/blog",
+      lastModified: new Date(),
     },
     {
       url: "http://ekoseon.fr/foire-aux-questions",
+      lastModified: new Date(),
     },
     {
       url: "http://ekoseon.fr/mentions-legales",
+      lastModified: new Date(),
     },
     {
       url: "http://ekoseon.fr/politique-de-confidentialite",
+      lastModified: new Date(),
     },
     {
       url: "http://ekoseon.fr/conditions-generales-de-vente",
+      lastModified: new Date(),
     },
   ];
-    
-
-  
-
 
   const baseUrl = process.env.NEXT_PUBLIC_RELATIVE_URI;
 
   const posts = await getBlogPosts({ publishedOnly: true });
-  
+
   const postsUrls =
     posts?.map((post) => {
       return {
